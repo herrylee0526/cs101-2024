897d455580d08bd3b2898a711c879efb05eb2c22
herrylee0526 <76048221+herrylee0526@users.noreply.github.com>
2024-05-09 14:15:49 +0800

    Add files via upload

---
+++ ex4/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Fueltank {
+private:
+    int m_FueltankCapacity;
+    int m_Gas_grade;
+public:
+    Fueltank(int FueltankCapacity = 3000, int Gas = 98): m_FueltankCapacity(FueltankCapacity), m_Gas_grade(Gas) {}
+    
+    void fuel_up(int v, int gas) {
+        if (v > m_FueltankCapacity) {
+            cout << "Error: FueltankCapacity: 3000 but fuel up " << v << endl<<"fuel_up:"<<v<<" ";
+        } else {
+            cout << "fuel_up: " << v << endl; 
+        }
+        
+        if (gas != 95) {
+            cout <<"Error Gas_grade:"<<gas<<" "<< "Correct Gas_grade: 95" << endl;
+        } else {
+            cout << "Gas_grade = " << gas << endl;
+        }
+    }
+    
+    void set_Gas_grade(int Gas_grade) { 
+        m_Gas_grade = Gas_grade;
+        cout << "set_Gas_grade=" << Gas_grade << endl; 
+    }
+    
+    int get_Gas_grade() {
+        return m_Gas_grade;
+    }
+};
+
+int main() {
+    cout << "Constructing AUDI_Car" << endl;
+    cout << "Audi: Gas_grade = 98" << endl;
+    
+    Fueltank audi;
+    audi.set_Gas_grade(95);
+    cout << "Audi: Gas_grade = " << audi.get_Gas_grade() << endl;
+    audi.fuel_up(300, 95);
+    
+    return 0;
+}
